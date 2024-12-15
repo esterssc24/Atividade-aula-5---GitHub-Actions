1ec251fdc9e72b56fc43310b26ac878a23ba5104
esterssc24 <ester.ssc@hotmail.com>
2024-12-15 20:59:50 -0300

    Exclusão do diretório Atividade-aula-5---GitHub-Actions

---
+++ test_conversao.py
@@ -0,0 +1,26 @@
+import unittest
+from conversao import metros_para_km, metros_para_milhas, km_para_metros, milhas_para_metros
+
+class TestConversao(unittest.TestCase):
+    def test_metros_para_km(self):
+        self.assertEqual(metros_para_km(1000), 1)
+        self.assertEqual(metros_para_km(500), 0.5)
+        self.assertEqual(metros_para_km(1500), 1.5)
+    
+    def test_metros_para_milhas(self):
+        self.assertEqual(metros_para_milhas(1609.34), 1)
+        self.assertEqual(metros_para_milhas(1000), 0.621371)
+        self.assertEqual(metros_para_milhas(500), 0.310685)
+    
+    def test_km_para_metros(self):
+        self.assertEqual(km_para_metros(1), 1000)
+        self.assertEqual(km_para_metros(0.5), 500)
+        self.assertEqual(km_para_metros(2), 2000)
+    
+    def test_milhas_para_metros(self):
+        self.assertEqual(milhas_para_metros(1), 1609.34)
+        self.assertEqual(milhas_para_metros(0.5), 804.67)
+        self.assertEqual(milhas_para_metros(2), 3218.68)
+
+if __name__ == '__main__':
+    unittest.main()

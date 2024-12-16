bb4eb71170cecd34c268152cd9846e18e0a859e9
esterssc24 <ester.ssc@hotmail.com>
2024-12-15 21:15:53 -0300

    Adicionando nova pasta ao repositório

---
+++ conversao.py
@@ -0,0 +1,15 @@
+def metros_para_km(metros):
+    """Converte metros para quilômetros."""
+    return metros / 1000
+
+def metros_para_milhas(metros):
+    """Converte metros para milhas."""
+    return metros * 0.000621371
+
+def km_para_metros(km):
+    """Converte quilômetros para metros."""
+    return km * 1000
+
+def milhas_para_metros(milhas):
+    """Converte milhas para metros."""
+    return milhas / 0.000621371
